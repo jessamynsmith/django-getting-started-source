92e800d0d7f81b607d9637a0039121028e0573b6
Jessamyn Smith <geekchick77@gmail.com>
2020-05-04 20:47:34 -0700

    Updated requirements

--- my_project/settings.py
+++ my_project/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -76,10 +78,9 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default="sqlite:///%s" % os.path.join(BASE_DIR, 'eggtimer', 'eggtimer.sqlite')
+    )
 }
 
 
